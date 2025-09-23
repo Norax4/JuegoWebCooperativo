a2d8ac8fc5eef01b6a8db1616efb88a1a851a0b5
jcar512 <209171620+jcar5@users.noreply.github.com>
2025-09-23 03:53:10 -0300

    test generador rng

--- Coopera.Tests/UnitTests/Models/ValidationTests.cs
+++ Coopera.Tests/UnitTests/Models/ValidationTests.cs
@@ -1,13 +1,30 @@
+using Coopera.Models;
+using Coopera.Enums;
+
 namespace Coopera.Tests.UnitTests.Models;
 
 public class ValidationTests
 {
     [Theory]
-    [InlineData(22, 22)]
-    public void ChequearGeneradorPorSeeds(int input, int esperado)
+    [InlineData(22, 14, Dificultad.Facil)]
+    [InlineData(4234, 24, Dificultad.Facil)]
+    [InlineData(932, 23, Dificultad.Facil)]
+    [InlineData(8812, 17, Dificultad.Facil)]
+    [InlineData(7326, 10, Dificultad.Facil)]
+    [InlineData(22, 34, Dificultad.Media)]
+    [InlineData(4234, 44, Dificultad.Media)]
+    [InlineData(932, 43, Dificultad.Media)]
+    [InlineData(8812, 37, Dificultad.Media)]
+    [InlineData(7326, 30, Dificultad.Media)]
+    [InlineData(22, 61, Dificultad.Dificil)]
+    [InlineData(4234, 84, Dificultad.Dificil)]
+    [InlineData(932, 81, Dificultad.Dificil)]
+    [InlineData(8812, 67, Dificultad.Dificil)]
+    [InlineData(7326, 50, Dificultad.Dificil)]
+    public void ChequearGeneradorDeSiempreIgualPasandoMismosSeeds(int input, int esperado, Dificultad dificultad)
     {
         //Arrange
-        Partida partida = new(1);
+        Partida partida = new(dificultad);
         int resultadoEsperado = esperado;
 
         //Act
