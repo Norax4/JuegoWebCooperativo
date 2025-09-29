7c9f1295c8080f415dfd4d0dc814f58196b57909
Norax4 <angenoramurteza001@gmail.com>
2025-09-29 18:48:06 -0300

    Sincronizing

--- Coopera/Controllers/HomeController.cs
+++ Coopera/Controllers/HomeController.cs
@@ -18,8 +18,6 @@ namespace Coopera.Controllers
 			return View();
 			}
 
-		// public IActionResult CrearJugador(string nombre){}
-
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 			{
